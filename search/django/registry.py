05305d62dda4060ea1f827a57edf1dbddd9d6858
Martin Allison <martinallison@gmail.com>
2016-08-11 12:26:06 +0100

    Tidy up and docstring

--- search/django/registry.py
+++ search/django/registry.py
@@ -1,7 +1,11 @@
-class RegisterError(Exception): pass
+class RegisterError(Exception):
+    pass
 
 
 class __Registry(dict):
+    """Registry for model -> document class mappings. Raises an error if a model
+    tries to be registered twice.
+    """
     def __setitem__(self, model_class, document_class):
         cls = self.setdefault(model_class, document_class)
         if cls != document_class:
@@ -9,5 +13,6 @@ class __Registry(dict):
                 u'Cannot register {} for model {} already registered to'
                 u' {}'.format(document_class, model_class, cls)
             )
+
 
 registry = __Registry()
